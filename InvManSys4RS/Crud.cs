c531fab838755857853889629dd3351ac8a0dfdb
Tomek <Tomek@KOMPUTER>
2023-05-17 23:59:21 +0200

    tables added

---
+++ InvManSys4RS/Crud.cs
@@ -0,0 +1,42 @@
+﻿using InvManSys4RS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvManSys4RS
+{
+    class Crud
+    {
+        public string Read(string table)
+        {
+            string mes = "";
+            string readLine = "";
+            List<string> listRead = new List<string>();
+
+            using (var context = new LibraryDb1Context())
+            {
+                var borrowers = context.Borrowers.ToList();
+                foreach (var borrower in borrowers)
+                {
+                    readLine=$"BorrowerID: {borrower.BorrowerId}| Name: {borrower.Name}| Email: {borrower.Email}| TotalBorrowedBooks: {borrower.TotalBorrowedBooks}";
+                    listRead.Add(readLine);
+                }
+
+                StringBuilder sb = new StringBuilder();
+                foreach (string item in listRead)
+                {
+                    sb.AppendLine(item);
+                }
+
+                mes = sb.ToString();
+
+              
+            }
+
+            //string TestMessage = $"You pressed the read button with {table} chosen maaan";          
+            return mes;
+        }
+    }
+}
--- InvManSys4RS/MainWindow.xaml.cs
+++ InvManSys4RS/MainWindow.xaml.cs
@@ -24,10 +24,51 @@ namespace InvManSys4RS
         {
             InitializeComponent();
         }
+        //1. DB Created
+        //2. EF Core installed
+        //3. Model Classes generated
 
+        //variables declaration
+        string chosenTable = "Inventory";
+        //add
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+        //delete
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //edit
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //display
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Crud crud = new Crud();
+            txtMainBox.Text = crud.Read(table);
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            chosenTable = "Inventory";
+            txtInfo.Text = chosenTable;
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            chosenTable = "Suppliers";
+            txtInfo.Text = chosenTable;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
         }
     }
 }
